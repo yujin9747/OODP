c06f1a4a3dafd86d7499fe1764f93e4f342e2b8b
Samuel Cho <tkandpf919@gmail.com>
2023-04-23 17:40:09 +0900

    implement search, logout Action listener

--- src/main/java/com/example/demo/jframe/MainWindow.java
+++ src/main/java/com/example/demo/jframe/MainWindow.java
@@ -5,15 +5,19 @@ import com.example.demo.domain.Library;
 import com.example.demo.domain.Role;
 import com.example.demo.domain.Student;
 import com.example.demo.domain.Member;
+import com.example.demo.repository.BookRepository;
 import com.example.demo.repository.StudentRepository;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class MainWindow extends JFrame{
 
+    final int GUEST = -1;
+    static BookRepository bookRepository = new BookRepository();
     static StudentRepository studentRepository = new StudentRepository();
 
     Button searchBTN;
@@ -45,22 +49,25 @@ public class MainWindow extends JFrame{
 
         add(searchBTN);
 
+        searchBTN.addActionListener(new SearchActionListener());
+
         if (indexOfMember == -1) {
             loginBTN = new Button("Login");
             loginBTN.setBounds(300, 300, 70, 30);
             add(loginBTN);
+            loginBTN.addActionListener(new LoginActionListener());
         } else {
             logoutBTN = new Button("Logout");
             logoutBTN.setBounds(300, 300, 70, 30);
+            logoutBTN.addActionListener(new LogoutActionListener());
             add(logoutBTN);
             if(loginedMember.getRole() != Role.ADMIN){
                 adminPageBTN = new Button("Admin Page");
                 adminPageBTN.setBounds(300, 350, 70, 30);
                 add(adminPageBTN);
+                // TODO: adminPage direction
             }
         }
-
-        searchBTN.addActionListener(new SearchActionListener());
 
         setVisible(true); //보이기
 
@@ -69,7 +76,18 @@ public class MainWindow extends JFrame{
     private class SearchActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            new SearchWindow(indexOfMember, 0);
+            String command = e.getActionCommand();
+            String bookTitle = searchBoxField.getText();
+            int indexOfBook = -1;
+
+            List<Book> bookList = bookRepository.getBookList();
+            for(int i=0; i<bookList.size(); i++) {
+                if(bookList.get(i).getTitle().equals(bookTitle)) {
+                    indexOfBook = i;
+                }
+            }
+            System.out.println("book index: " + indexOfBook);
+            new SearchWindow(indexOfMember, indexOfBook);
             setVisible(false);
         }
     }
@@ -78,6 +96,14 @@ public class MainWindow extends JFrame{
         @Override
         public void actionPerformed(ActionEvent e) {
             new LoginWindow();
+            setVisible(false);
+        }
+    }
+
+    private class LogoutActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new MainWindow(-1);
             setVisible(false);
         }
     }
