54b2a997ce297f13f25cbd2f71757dfa73addab1
Samuel Cho <tkandpf919@gmail.com>
2023-04-23 17:00:07 +0900

    button verify 조건 설정

--- src/main/java/com/example/demo/jframe/MainWindow.java
+++ src/main/java/com/example/demo/jframe/MainWindow.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.Library;
 import com.example.demo.domain.Role;
 import com.example.demo.domain.Student;
 import com.example.demo.domain.Member;
+import com.example.demo.repository.StudentRepository;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,14 +13,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame{
+
+    static StudentRepository studentRepository = new StudentRepository();
+
     Button searchBTN;
-
     Button loginBTN;
-
     Button adminPageBTN;
-    JTextField searchBoxField = new JTextField("책 제목을 입력하세요", 20);;
+    Button logoutBTN;
 
-    public MainWindow(int userIndex) { //생성자를 만든다.
+    JTextField searchBoxField = new JTextField("책 제목을 입력하세요", 20);
+
+    int indexOfMember;
+    Member loginedMember;
+
+    public MainWindow(int indexOfMember) { //생성자를 만든다.
+        this.indexOfMember = indexOfMember;
+        this.loginedMember = (indexOfMember == -1) ? null : studentRepository.getStudentList().get(indexOfMember);
 
         setTitle("Main"); //창 제목
         setSize(600, 600); //창 사이즈
@@ -31,20 +40,25 @@ public class MainWindow extends JFrame{
         c.setLayout(new FlowLayout());
         c.add(searchBoxField);
 
-        //button field
-
-        //if user is admin
-        adminPageBTN = new Button("Admin Page");
-        adminPageBTN.setBounds(300, 350, 70, 30);
-
-        loginBTN = new Button("Login");
-        loginBTN.setBounds(300, 300, 70, 30);
-
         searchBTN = new Button("search");
         searchBTN.setBounds(20, 5, 70, 30);
 
         add(searchBTN);
-        add(loginBTN);
+
+        if (indexOfMember == -1) {
+            loginBTN = new Button("Login");
+            loginBTN.setBounds(300, 300, 70, 30);
+            add(loginBTN);
+        } else {
+            logoutBTN = new Button("Logout");
+            logoutBTN.setBounds(300, 300, 70, 30);
+            add(logoutBTN);
+            if(loginedMember.getRole() != Role.ADMIN){
+                adminPageBTN = new Button("Admin Page");
+                adminPageBTN.setBounds(300, 350, 70, 30);
+                add(adminPageBTN);
+            }
+        }
 
         searchBTN.addActionListener(new SearchActionListener());
 
@@ -55,7 +69,7 @@ public class MainWindow extends JFrame{
     private class SearchActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            new LoginWindow(); // SearchWindow로 변경 예정
+            new SearchWindow(indexOfMember, 0);
             setVisible(false);
         }
     }
