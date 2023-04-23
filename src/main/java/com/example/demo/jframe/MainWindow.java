35666afb879a0f022c37b99c60ba6e71398a6dcc
Samuel Cho <tkandpf919@gmail.com>
2023-04-23 16:38:43 +0900

    add admin page button

--- src/main/java/com/example/demo/jframe/MainWindow.java
+++ src/main/java/com/example/demo/jframe/MainWindow.java
@@ -15,9 +15,11 @@ public class MainWindow extends JFrame{
     Button searchBTN;
 
     Button loginBTN;
+
+    Button adminPageBTN;
     JTextField searchBoxField = new JTextField("책 제목을 입력하세요", 20);;
 
-    public MainWindow() { //생성자를 만든다.
+    public MainWindow(int userIndex) { //생성자를 만든다.
 
         setTitle("Main"); //창 제목
         setSize(600, 600); //창 사이즈
@@ -28,6 +30,12 @@ public class MainWindow extends JFrame{
 
         c.setLayout(new FlowLayout());
         c.add(searchBoxField);
+
+        //button field
+
+        //if user is admin
+        adminPageBTN = new Button("Admin Page");
+        adminPageBTN.setBounds(300, 350, 70, 30);
 
         loginBTN = new Button("Login");
         loginBTN.setBounds(300, 300, 70, 30);
@@ -61,7 +69,7 @@ public class MainWindow extends JFrame{
     }
     public static void main(String[] args) {
 
-        new MainWindow(); //생성자 호출
+        new MainWindow(-1); //생성자 호출
     }
 
 }
