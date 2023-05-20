8212d9a764234bc59fe60be291e304d06307b40d
Samuel Cho <tkandpf919@gmail.com>
2023-05-21 07:10:28 +0900

    add functions to service

--- src/main/java/com/example/demo/service/ReservationInfoService.java
+++ src/main/java/com/example/demo/service/ReservationInfoService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Member;
+import com.example.demo.domain.RentalInfo;
 import com.example.demo.domain.ReservationInfo;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.ReservationInfoRepository;
@@ -19,4 +20,8 @@ public class ReservationInfoService {
     public void saveReservationInfo(ReservationInfo reservationInfo){
         reservationInfoRepository.save(reservationInfo);
     }
+
+    public ReservationInfo findOne(Long reservationInfoId){ return reservationInfoRepository.findOne(reservationInfoId);}
+    public ReservationInfo findOneByMemberIdAndBookId(Long memberId, Long bookId){return reservationInfoRepository.findOneByMemberIdAndBookId(memberId, bookId);}
+    public Long cancelReservation(ReservationInfo reservationInfo){return reservationInfoRepository.delete(reservationInfo);}
 }
