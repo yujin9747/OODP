b9d5bdcf3411ccb07ab204d1d808a875a390e42d
wallabee999 <131531574+wallabee999@users.noreply.github.com>
2023-05-31 18:58:21 +0900

    Create Builder.java

---
+++ src/main/java/com/example/demo/jframe/Builder.java
@@ -0,0 +1,19 @@
+package com.example.demo.jframe;
+
+import com.example.demo.BeanUtil;
+import com.example.demo.domain.*;
+
+import com.example.demo.domain.request.BookUpdateForm;
+import com.example.demo.service.BookService;
+import com.example.demo.service.MemberService;
+import com.example.demo.service.RentalInfoService;
+import com.example.demo.service.ReservationInfoService;
+public class Builder {
+    public interface PageBuilder{
+
+
+
+
+
+    }
+}
