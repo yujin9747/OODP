3e48dfcbc30e553c64002ead714050d0b8d6ef16
wallabee999 <131531574+wallabee999@users.noreply.github.com>
2023-05-31 20:32:34 +0900

    Builder

--- src/main/java/com/example/demo/jframe/Builder.java
+++ src/main/java/com/example/demo/jframe/Builder.java
@@ -1,19 +1,26 @@
-package com.example.demo.jframe;
-
-import com.example.demo.BeanUtil;
-import com.example.demo.domain.*;
-
-import com.example.demo.domain.request.BookUpdateForm;
-import com.example.demo.service.BookService;
-import com.example.demo.service.MemberService;
-import com.example.demo.service.RentalInfoService;
-import com.example.demo.service.ReservationInfoService;
-public class Builder {
-    public interface PageBuilder{
-
-
-
-
-
-    }
-}
+//package com.example.demo.jframe;
+//
+//import com.example.demo.domain.Book;
+//import com.example.demo.domain.Member;
+//
+//import javax.swing.*;
+//
+//public interface Builder {
+//    Builder loginedMember(Member loginedMember);
+//    Builder searchedBook(Book searchedBook);
+//    Builder selectedBook(Book selectedBook);
+//    Builder beforePage(Integer beforePage);
+//    Builder selectedIdx(Integer selectedIdx);
+//    Builder editMode(boolean editMode);
+//    Builder loginOrRegister(int loginOrRegister);
+//    Builder listModel(DefaultListModel listModel);
+//    Builder description(String description);
+//    MainWindow getMainWindow();
+//    UserManageWindow getUserManageWindow();
+//    UserPageWindow getUserPageWindow();
+//    SearchWindow getSearchWindow();
+//    Builder getMain(Member loginedMember);
+//    Builder getLoginWindow(int loginOrRegister);
+//    Builder getAdminManagement(Member loginedMember, Book selectedBook, Integer selectedIdx);
+//
+//}
