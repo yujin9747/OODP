24b6c4b0a2eba83a2a5c50be82160a6896241b89
wallabee999 <131531574+wallabee999@users.noreply.github.com>
2023-05-31 21:13:53 +0900

    Update MainWindowBuilder.java

---
+++ src/main/java/com/example/demo/builder/builder/MainWindowBuilder.java
@@ -0,0 +1,10 @@
+package com.example.demo.builder.builder;
+
+import com.example.demo.domain.Member;
+
+public interface MainWindowBuilder {
+
+    MainWindowBuilder MainWindow(Member loginedMember);
+
+
+}
